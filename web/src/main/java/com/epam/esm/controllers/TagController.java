0e9f1e7fc715fa5ed25307e615816cb9803f1165
Evgeniy <koroza1995@gmail.com>
2021-12-13 18:29:16 +0300

    *

--- web/src/main/java/com/epam/esm/controllers/TagController.java
+++ web/src/main/java/com/epam/esm/controllers/TagController.java
@@ -10,12 +10,14 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping(value = "/tag", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -29,33 +31,53 @@ public class TagController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Boolean> addTagDto(@RequestBody TagDto tagDto) throws InvalidEntityDataException, EntityAlreadyExistsException {
+    public ResponseEntity<Boolean> addTagDto(@RequestBody TagDto tagDto,
+                                             @RequestParam(value = "loc", required = false) String locale)
+            throws InvalidEntityDataException, EntityAlreadyExistsException {
         logger.log(Level.DEBUG, "method addTagDto()");
+        setLocale(locale);
         boolean result = service.add(tagDto);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TagDto> getTagDtoById(@PathVariable("id") long id) throws EntityNotFoundException {
+    public ResponseEntity<TagDto> getTagDtoById(@PathVariable("id") long id,
+                                                @RequestParam(value = "loc", required = false) String locale)
+            throws EntityNotFoundException {
         logger.log(Level.DEBUG, "method getTagDtoById()");
+        setLocale(locale);
         return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
     }
 
     @GetMapping("/name/{partOfTagName}")
-    public ResponseEntity<List<TagDto>> findByPartOfTagName(@PathVariable String partOfTagName) {
+    public ResponseEntity<List<TagDto>> findByPartOfTagName(@PathVariable String partOfTagName,
+                                                            @RequestParam(value = "loc", required = false) String locale) {
         logger.log(Level.DEBUG, "method findByPartOfTagName()");
+        setLocale(locale);
         return new ResponseEntity<>(service.findByPartOfName(partOfTagName), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> delete(@PathVariable("id") long id) throws EntityNotFoundException {
+    public ResponseEntity<Boolean> delete(@PathVariable("id") long id,
+                                          @RequestParam(value = "loc", required = false) String locale)
+            throws EntityNotFoundException {
         logger.log(Level.DEBUG, "method delete()");
+        setLocale(locale);
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 
     @GetMapping("/certificates/{id}")
-    public ResponseEntity<List<GiftCertificateDto>> findTagCertificates(@PathVariable long id) throws EntityNotFoundException {
+    public ResponseEntity<List<GiftCertificateDto>> findTagCertificates(@PathVariable long id,
+                                                                        @RequestParam(value = "loc", required = false) String locale)
+            throws EntityNotFoundException {
         logger.log(Level.DEBUG, "method findTagCertificates()");
+        setLocale(locale);
         return new ResponseEntity<>(service.findTagCertificates(id), HttpStatus.OK);
+    }
+
+    private void setLocale(String locale) {
+        if (locale !=null) {
+            LocaleContextHolder.setLocale(new Locale(locale));
+        }
     }
 }
