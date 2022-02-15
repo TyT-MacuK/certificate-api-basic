e1053aa2fa81dfe428f66d0993c14d69b2e0a9fd
Evgeniy <koroza1995@gmail.com>
2022-02-15 16:32:01 +0300

    fix

--- repository/src/main/java/com/epam/esm/dao/TagDao.java
+++ repository/src/main/java/com/epam/esm/dao/TagDao.java
@@ -16,13 +16,15 @@ public interface TagDao extends JpaRepository<Tag, Long> {
     @Query(value = "SELECT t FROM Tag t WHERE t.name = :name")
     Optional<Tag> findByName(@Param("name") String name);
 
-    @Query(value = "SELECT tag.id, tag.name FROM Tag" +
-            "JOIN gift_certificate_has_tag ON gift_certificate_has_tag.tag_id = tag.id" +
-            "JOIN gift_certificate ON gift_certificate.id = gift_certificate_has_tag.gift_certificate_id" +
-            "JOIN orders ON orders.gift_certificate_id = gift_certificate.id" +
-            "JOIN user ON user.id = orders.user_id" +
-            "WHERE user.id = :id" +
-            "GROUP BY orders.gift_certificate_id" +
-            "ORDER BY COUNT(*) DESC LIMIT 1", nativeQuery = true)
+        @Query(value = """
+            SELECT tag.id, tag.name FROM Tag
+            JOIN gift_certificate_has_tag ON gift_certificate_has_tag.tag_id = tag.id
+            JOIN gift_certificate ON gift_certificate.id = gift_certificate_has_tag.gift_certificate_id
+            JOIN orders ON orders.gift_certificate_id = gift_certificate.id
+            JOIN user ON user.id = orders.user_id
+            WHERE user.id = :id
+            GROUP BY orders.gift_certificate_id
+            ORDER BY COUNT(*) DESC LIMIT 1
+            """, nativeQuery = true)
     Optional<Tag> findMostWidelyUsedTag(@Param("id") Long userId);
 }
